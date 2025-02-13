1da4c2c6d04dee07ca97ddc3a22421bd9d9af687
Ma.Christina Marbani <marbanimachristina_bsit@plmun.edu.ph>
2025-02-13 22:55:32 +0800

    Submitting Python Week 04 - Laboratory # 10

--- if_elif_else_statement.py
+++ if_elif_else_statement.py
@@ -13,5 +13,8 @@ elif grade >= 60:
     letter_grade = "D"
 else:
     letter_grade = "F"
+    
+   print("Your grade is:", letter_grade)
+   
 except ValueError:
     print("Invalid input. Please enter an integer.")
